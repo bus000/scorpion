245f7bd087d79caa02b779db3b7e42f48843d2a7
Pelle Juul Christensen <pelle@Pelles-MacBook-Pro.local>
2015-09-25 09:24:04 +0200

    Add camera calibration utility and some additions to locator-test.

--- opencv_test/locator-test.cpp
+++ opencv_test/locator-test.cpp
@@ -1,5 +1,9 @@
 #include "locator.hpp"
 
+double distance(double height, double realHeight) {
+  return 1034.246244 * (realHeight / height);
+}
+
 int main(int argc, char **argv) {
     VideoCapture capture(0);
     Mat frame;
@@ -18,19 +22,26 @@ int main(int argc, char **argv) {
             break;
         }
 
+        double height = 0;
+        double dist = 0;
+
         dest = Mat::zeros(frame.size(), CV_8UC3);
         vector<Point> *hull = getHull(frame);
         if (hull != NULL) {
-            drawHull(*hull, Scalar(255, 255, 255), dest);
+            drawHull(*hull, Scalar(255, 255, 0), frame);
             Point center = hullCenter(*hull);
-            circle(dest, center, 20, Scalar(0, 0, 255), 3);
+            // circle(dest, center, 20, Scalar(0, 0, 255), 3);
+
+            height = hullHeight(*hull);
+            dist   = distance(height, 0.21);
+            printf("Height (px): %f\tDistance (m): %f\n", height, dist);
         }
         
-        imshow("videoWindow", dest);
+        resize(frame, frame, Size(), 0.5, 0.5); 
+        imshow("videoWindow", frame);
 
         int key = waitKey(30);
         if(key == 27)
-            break;
         delete hull;
     }
 
