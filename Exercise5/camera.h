4d40eb118cd8ffc090a5f0e4f7cb2d30e7907b61
Bus000 <magnus@stavngaard.dk>
2015-10-20 15:45:41 +0200

    Fix includes

--- Exercise5/camera.h
+++ Exercise5/camera.h
@@ -1,8 +1,11 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
-#include "cxcore.h"
-#include "highgui.h"
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 #include <string>
 
