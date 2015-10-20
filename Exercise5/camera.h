2ba7164d078474b1d821de3ab5b1e18a8ec23960
Bus000 <magnus@stavngaard.dk>
2015-10-20 15:46:19 +0200

    Whitespace fix

--- Exercise5/camera.h
+++ Exercise5/camera.h
@@ -17,7 +17,7 @@ namespace object
     horizontal,
     vertical
   };
-  
+
   std::string name (type t);
 }
 
@@ -32,36 +32,36 @@ class camera
     // Constructor / Destructor
     camera (const int idx = -1);
     ~camera ();
-    
+
     // Accessor for the OpenCV camera handle
     CvCapture* get_capture ();
-    
+
     // Image acquisition
     IplImage* get_colour ();
     IplImage* get_grey (IplImage *colour = NULL);
-    
+
     // Object detection
     object::type get_object (IplImage *im, colour_prop &p, double &distance, double &angle);
     void draw_object (IplImage *im);
 
     // Low-level object detection
     CvPoint2D32f* get_corners (IplImage *im, bool &found, int &corner_count);
-  
+
   private:
     // Object parameters
     const CvSize pattern_size;
     const double patternUnit; // Size of one checker square in mm
-    
+
     // Camera handle
     CvCapture *cam;
-    
+
     // Camera calibration parameters
     CvMat *intrinsic_matrix, *distortion_coeffs, *mapx, *mapy;
-    
+
     // Buffers
     IplImage *grey, *colour, *mask;
     CvPoint2D32f *corners;
-    
+
     // States for object detection
     bool found;
     int corner_count;
