3d6d167222bb8c42d4d58821fc4efa6384d667e2
PelleJuul <pelle@stand.dk>
2015-10-20 15:14:09 +0200

    Moved DriveCtl.* to Exercise5

---
+++ Exercise5/DriveCtl.hpp
@@ -0,0 +1,46 @@
+#ifndef DRIVE_CTL_H
+#define DRIVE_CTL_H
+
+#define DRIVE_TIME 10000000/96.5
+#define TURN_TIME 61100000/(2*360)
+#define DRIVE_FORWARD 1
+#define DRIVE_BACKWARD -1
+#include <libplayerc++/playerc++.h>
+
+
+class DriveCtl{
+public:
+    DriveCtl(PlayerCc::PlayerClient *robot, PlayerCc::Position2dProxy *position);
+
+    void drive(double cm);
+    void turn(double degrees);
+    void turnLeft(double degrees);
+    void turnRight(double degrees);
+    void setDirection(int direction);
+    void setXPos(double x);
+    void setYPos(double y);
+    void setYaw(double yaw);
+    double getXPos();
+    double getYPos();
+    double getYaw();
+    void goToPos(double x, double y);
+    void resetCounters();
+    void resetYawed();
+    void resetDriven();
+    double getYawed();
+    double getDriven();
+
+private:
+    PlayerCc::PlayerClient *robot;
+    PlayerCc::Position2dProxy *position;
+    double direction;
+    double speed;
+    double turnSpeed;
+    double toRadians(double degrees);
+    double toDegrees(double radians);
+    double xPos, yPos, yaw;
+    double yawed, driven;
+    void overRunYaw();
+};
+
+#endif
