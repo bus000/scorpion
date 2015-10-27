4c170f1880c48b1b5e1b536d67df49e4070bc39b
Bus000 <magnus@stavngaard.dk>
2015-10-27 19:41:15 +0100

    Make it more clear what file is used for what distance

--- exam_2015/scripts/analyseSensor.py
+++ exam_2015/scripts/analyseSensor.py
@@ -4,23 +4,29 @@ import numpy
 import matplotlib.pyplot as plt
 import os
 
-datafolder = '../data/'
+# Distance, datafile
+datafiles = []
+datafiles.append({'dist': 5,  'name': '../data/05cm.dat'})
+datafiles.append({'dist': 10, 'name': '../data/10cm.dat'})
+datafiles.append({'dist': 15, 'name': '../data/15cm.dat'})
+datafiles.append({'dist': 20, 'name': '../data/20cm.dat'})
+datafiles.append({'dist': 25, 'name': '../data/25cm.dat'})
+datafiles.append({'dist': 30, 'name': '../data/30cm.dat'})
+datafiles.append({'dist': 35, 'name': '../data/35cm.dat'})
+datafiles.append({'dist': 40, 'name': '../data/40cm.dat'})
 
 sums = []
-for datafile in sorted(os.listdir(datafolder)):
-    print('handling', datafile)
-
-    data = numpy.loadtxt(datafolder+datafile, delimiter=':', usecols=range(1,2))
+for datafile in datafiles:
+    data = numpy.loadtxt(datafile.get('name'), delimiter=':', usecols=range(1,2))
 
     running_sum = 0
     for num in data:
         running_sum = running_sum + num
 
-    sums.append((datafile, running_sum / len(data)))
-
+    sums.append((datafile.get('dist'), running_sum / len(data)))
 
 for s in sums:
-    plt.plot(s[0][:2], s[1], 'ro')
+    plt.plot(s[0], s[1], 'ro')
     print (s[0], s[1])
 
 plt.axis([0, 45, 0, 1.6])
